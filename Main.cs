69fc390c51f7106b10587d67b77c422f5fdb3485
Hoshino Starry <admin@error063.work>
2024-09-04 21:13:47 +0800

    恢复NetworkLogger

--- Main.cs
+++ Main.cs
@@ -89,7 +89,7 @@ namespace SinmaiAssist
             }
             if (config.DisableMask) Patch(typeof(DisableMask));
             if (config.SinglePlayer) Patch(typeof(SinglePlayer));
-            // if (config.NetworkLogger) Patch(typeof(NetworkLogger));
+            if (config.NetworkLogger) Patch(typeof(NetworkLogger));
             if (config.ForwardATouchRegionToButton) Patch(typeof(ForwardATouchRegionToButton));
             //if (config.ForceCurrentIsBest) Patch(typeof(ForceCurrentIsBest));
             if (config.DisableReboot) Patch (typeof(DisableReboot));
