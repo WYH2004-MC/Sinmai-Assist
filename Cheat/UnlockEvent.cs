2a0aaa2182aa0f753aa94f7c7ff40710c9a7d53f
Hoshino Starry <admin@error063.work>
2024-09-04 18:56:26 +0800

    游戏活动解锁功能的改进
    对UnlockEvent.cs进行了必要的更改，以正确处理游戏活动的解锁。此更新修改了遍历目录的方法，以包含特定的文件夹结构，并确保正确解析活动ID。此外，调整了方法签名以保持一致性。

--- Cheat/UnlockEvent.cs
+++ Cheat/UnlockEvent.cs
@@ -14,24 +14,36 @@ namespace Cheat
     {
         [HarmonyPostfix]
         [HarmonyPatch(typeof(PacketGetGameEvent), "Proc")]
-        public static void Unlock(PacketGetGameEvent __instance, ref PacketState __result)
+        public static void Unlock(PacketGetGameEvent instance, ref PacketState result)
         {
-            if (__result != PacketState.Done) return;
-            NetQuery<GameEventRequestVO, GameEventResponseVO> netQuery = __instance.Query as NetQuery<GameEventRequestVO, GameEventResponseVO>;
+            if (result != PacketState.Done) return;
+            NetQuery<GameEventRequestVO, GameEventResponseVO> netQuery = instance.Query as NetQuery<GameEventRequestVO, GameEventResponseVO>;
             List<GameEvent> list = new List<GameEvent>();
-            foreach (DirectoryInfo item2 in new DirectoryInfo("./Sinmai_Data/StreamingAssets/A000/event").EnumerateDirectories("*", SearchOption.AllDirectories))
+            foreach (DirectoryInfo optFolder in new DirectoryInfo("./Sinmai_Data/StreamingAssets").EnumerateDirectories("*", SearchOption.AllDirectories))
             {
-                int id = int.Parse(item2.Name.Replace("event", ""));
-                GameEvent item = default(GameEvent);
-                item.id = id;
-                item.startDate = "2000-01-01 00:00:00";
-                item.endDate = "2077-07-21 11:45:14";
-                item.type = 1;
-                list.Add(item);
+                if (optFolder.Name.StartsWith("A") && int.TryParse(optFolder.Name.Replace("A", ""), out var optNumber))
+                {
+                    foreach (DirectoryInfo eventFolder in new DirectoryInfo($"./Sinmai_Data/StreamingAssets/{optFolder.Name}/event").EnumerateDirectories("*", SearchOption.AllDirectories))
+                    {
+                        if (eventFolder.Name.StartsWith("event") &&
+                            int.TryParse(eventFolder.Name.Replace("event", ""), out var eventId))
+                        {
+                            GameEvent item = default(GameEvent);
+                            item.id = eventId;
+                            item.startDate = "2000-01-01 00:00:00";
+                            item.endDate = "2077-07-21 11:45:14";
+                            item.type = 1;
+                            list.Add(item);
+                        }
+                        
+                    }
+                }
+                
             }
+            
             netQuery.Response.gameEventList = list.ToArray();
             FieldInfo onDoneField = typeof(PacketGetGameEvent).GetField("_onDone", BindingFlags.NonPublic | BindingFlags.Instance);
-            Action<GameEvent[]> onDone = (Action<GameEvent[]>)onDoneField.GetValue(__instance);
+            Action<GameEvent[]> onDone = (Action<GameEvent[]>)onDoneField.GetValue(instance);
             onDone?.Invoke(netQuery.Response.gameEventList ?? Array.Empty<GameEvent>());
         }
     }
