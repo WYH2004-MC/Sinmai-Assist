575a48ab06615cf6763a7ff0de964104eb510331
WYH2004-MC <wangyuhao0824@outlook.com>
2024-09-07 14:18:32 +0800

    修改 ResetLoginBonusRecord实现方法

--- Cheat/ResetLoginBonusRecord.cs
+++ Cheat/ResetLoginBonusRecord.cs
@@ -1,26 +1,21 @@
-﻿using System;
-using System.Reflection;
-using HarmonyLib;
-using Net.Packet;
+﻿using HarmonyLib;
 using Net.Packet.Mai2;
-using Net.VO;
 using Net.VO.Mai2;
 
 namespace Cheat
 {
     public class ResetLoginBonusRecord
     {
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(PacketGetUserData), "Proc")]
-        public static void Proc(PacketGetUserData __instance, ref PacketState __result)
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PacketGetUserData), "SafeNullMember")]
+        public static bool SafeNullMember(PacketGetUserData __instance, UserDetailResponseVO src)
         {
-            if (__result != PacketState.Done) return;
-            NetQuery<UserDetailRequestVO, UserDetailResponseVO> netQuery = __instance.Query as NetQuery<UserDetailRequestVO, UserDetailResponseVO>;
-            netQuery.Response.userData.lastLoginDate = "2000-01-01 00:00:00";
-            netQuery.Response.userData.lastPairLoginDate = "2000-01-01 00:00:00";
-            FieldInfo onDoneField = typeof(PacketGetUserData).GetField("_onDone", BindingFlags.NonPublic | BindingFlags.Instance);
-            Action<UserDetail, int> onDone = (Action<UserDetail, int>)onDoneField.GetValue(__instance);
-            onDone?.Invoke(netQuery.Response.userData, netQuery.Response.banState);
+            if (src.userData.lastLoginDate != null)
+            {
+                src.userData.lastLoginDate = "2000-01-01 00:00:00";
+                src.userData.lastPairLoginDate = "2000-01-01 00:00:00";
+            }
+            return true;
         }
     }
 }
