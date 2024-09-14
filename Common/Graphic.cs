bd2c46fd841b5786a89c80bccd064e295da7a463
Hoshino Starry <admin@error063.work>
2024-09-14 16:03:44 +0800

    添加图形设置调整功能
    添加了新的图形调整工具，允许在运行时更改屏幕分辨率和帧率。此功能在`Common`命名空间内的`Graphic`类中实现，

---
+++ Common/Graphic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class Graphic
+    {
+        public static void ToggleFullscreen()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+
+        public static int GetResolutionWidth()
+        {
+            return Screen.width;
+        } 
+        public static int GetResolutionHeight()
+        {
+            return Screen.height;
+        }
+
+        public static void SetResolution(int width, int height)
+        {
+            Screen.SetResolution(width, height, Screen.fullScreen);
+        }
+
+        public static int GetMaxFrameRate()
+        {
+            return Application.targetFrameRate;
+        }
+
+        public static void SetMaxFrameRate(int fps)
+        {
+            Application.targetFrameRate = fps;
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
