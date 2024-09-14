2584db5181656aa5e60057797eb25f2248e55426
Hoshino Starry <admin@error063.work>
2024-09-14 14:00:46 +0800

    refactor(Common): 优化单人游戏相机位置和触摸面板校正
    重构SinglePlayer类以使用局部Camera变量，而不是每次调用Camera.main。调整相机位置和正交大小以符合单人游戏的配置。触摸面板校正现在基于相机的正交大小进行计算，以确保在隐藏子监视器时位置正确。

--- Common/HideSubMonitor.cs
+++ Common/HideSubMonitor.cs
@@ -26,30 +26,5 @@ namespace Common
 
             return false;
         }
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(MeshButton), "Awake")]
-        public static bool FixDummyTouchPanel(MeshButton __instance)
-        {
-            CustomGraphic customGraphic = __instance.targetGraphic as CustomGraphic;
-            FieldInfo touchAreaField = AccessTools.Field(typeof(MeshButton), "touchArea");
-            FieldInfo vertexArrayField = AccessTools.Field(typeof(MeshButton), "vertexArray");
-
-            touchAreaField.SetValue(__instance, (Manager.InputManager.TouchPanelArea)Enum.Parse(typeof(Manager.InputManager.TouchPanelArea), __instance.name));
-
-            Vector2[] vertexArray = new Vector2[customGraphic.vertex.Count];
-            for (int i = 0; i < customGraphic.vertex.Count; i++)
-            {
-                vertexArray[i] = RectTransformUtility.WorldToScreenPoint(
-                    Camera.main,
-                    new Vector2(
-                        __instance.transform.position.x + customGraphic.vertex[i].x,
-                        __instance.transform.position.y + customGraphic.vertex[i].y
-                    )
-                );
-            }
-            vertexArrayField.SetValue(__instance, vertexArray);
-            return false;
-        }
     }
 }
