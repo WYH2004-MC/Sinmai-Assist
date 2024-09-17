a92e0f78ad7abdc529fbd0cb0bd7abd8c1a7daa6
WYH2004-MC <32890512+WYH2004-MC@users.noreply.github.com>
2024-09-18 02:53:15 +0800

    修改 GetTitleServerUri方法,优化代码结构

--- Utils/Server.cs
+++ Utils/Server.cs
@@ -1,19 +1,37 @@
-﻿using MAI2.Util;
+﻿using System;
+using System.Collections.Generic;
+using MAI2.Util;
 using Manager;
 
 namespace Utils
 {
     public class Server
     {
+        private static readonly Dictionary<string, string> TitleServerMappings = new Dictionary<string, string>
+        {
+            { "maimai-gm.wahlap.com", "CN Wahlap Official" },
+            { "aquadx.hydev.org", "AquaDX Network" },
+            { "naominet.live", "RinNET" },
+            { "msm.moe", "Msm Aqua" }
+        };
+
         public static string GetTitleServerUri()
         {
             try
             {
                 string uri = Singleton<OperationManager>.Instance.GetBaseUri();
-                if (uri.Contains("maimai-gm.wahlap.com")) { return "CN Wahlap Official"; }
-                if (uri.Contains("aquadx.hydev.org")) { return "AquaDX Network"; }
+
+                foreach (var mapping in TitleServerMappings)
+                {
+                    if (uri.Contains(mapping.Key))
+                    {
+                        return mapping.Value;
+                    }
+                }
+
                 return uri;
-            }catch
+            }
+            catch (Exception e) when (e is NullReferenceException || e is InvalidOperationException)
             {
                 return "Unknown";
             }
