3769fbab5306435b7fe9e0030d43f714975fa7de
WYH2004-MC <32890512+WYH2004-MC@users.noreply.github.com>
2024-09-30 00:43:30 +0800

    更新 BuildInfo

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -1,6 +1,6 @@
 namespace SinmaiAssist { 
     public static partial class BuildInfo { 
-        public const string CommitHash = "d79070c"; 
-        public const string BuildDate = "2024-09-29T21:47:35.2876454+08:00"; 
+        public const string CommitHash = "2196223"; 
+        public const string BuildDate = "2024-09-30T00:19:20.6886613+08:00"; 
     } 
 } 
