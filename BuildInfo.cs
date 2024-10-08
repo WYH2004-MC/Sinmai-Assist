47dc2795509a5428ae390b26ae7db75d5a382adc
WYH2004-MC <32890512+WYH2004-MC@users.noreply.github.com>
2024-10-08 11:43:59 +0800

    修改 类名拼写错误

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -1,6 +1,6 @@
 namespace SinmaiAssist { 
     public static partial class BuildInfo { 
-        public const string CommitHash = "94085ee"; 
-        public const string BuildDate = "2024-10-08T11:26:39.6758422+08:00"; 
+        public const string CommitHash = "c23d5c7"; 
+        public const string BuildDate = "2024-10-08T11:55:00.8502037+08:00"; 
     } 
 } 
