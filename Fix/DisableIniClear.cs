46d8cd0541d558eceb20556c1d927a4b9f991912
WYH2004 <32890512+WYH2004-MC@users.noreply.github.com>
2025-06-09 08:31:38 +0800

    fix: 修复 ini 文件清空方法的识别，以正确识别 IniFile 类的clear方法

--- Fix/DisableIniClear.cs
+++ Fix/DisableIniClear.cs
@@ -6,7 +6,7 @@ namespace SinmaiAssist.Fix;
 public class DisableIniClear
 {
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(IniFile), "Clear")]
+    [HarmonyPatch(typeof(IniFile), "clear")]
     public static bool DisableClearMethod(IniFile __instance)
     {
         return false;
