feaa7c0cba9475abc2556e25b12bf0b84d6f90bc
WYH2004 <32890512+WYH2004-MC@users.noreply.github.com>
2025-06-12 01:55:20 +0800

    fix: 优化 CalcSpecialNum 方法，使用位运算设置结果

--- Fix/SkipSpecialNumCheck.cs
+++ Fix/SkipSpecialNumCheck.cs
@@ -1,25 +1,22 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
 using Manager;
+using MelonLoader;
 
 namespace SinmaiAssist.Fix;
 
 public class SkipSpecialNumCheck
 {
     // codes from AquaMai [https://github.com/MewoLab/AquaMai/blob/main/AquaMai.Mods/Fix/Common.cs]
-    public static void OnAfterPatch(HarmonyLib.Harmony h)
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameManager), "CalcSpecialNum")]
+    private static void CalcSpecialNum(ref int __result)
     {
-        if (typeof(GameManager).GetMethod("CalcSpecialNum") is null) return;
-        h.PatchAll(typeof(CalcSpecialNumPatch));
-    }
-
-    private class CalcSpecialNumPatch
-    {
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(GameManager), "CalcSpecialNum")]
-        private static bool CalcSpecialNum(ref int __result)
-        {
-            __result = 1024;
-            return false;
-        }
+#if DEBUG
+        MelonLogger.Warning($"[SkipSpecialNumCheck] OriginalSpecialNum:{__result.ToString()}, {Convert.ToString(__result, 2)}");
+#endif
+        var num = 0;
+        num |= 1024;
+        __result = num;
     }
 }
