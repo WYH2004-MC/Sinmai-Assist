d25f1e3f907e38e546ab86879dc6d3348ff953b8
GT610 <myddz1005@163.com>
2025-06-22 12:08:32 +0800

    feat: 增加键盘控制部分AutoPlay模式功能

--- GUI/AutoPlayPanel.cs
+++ GUI/AutoPlayPanel.cs
@@ -11,12 +11,27 @@ public class AutoPlayPanel
         AutoPlay.DisableUpdate = GUILayout.Toggle(AutoPlay.DisableUpdate, "Disable Mode Update");
         if (GUILayout.Button("Critical (AP+)", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Critical;
         if (GUILayout.Button("Perfect", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Perfect;
-        if (GUILayout.Button("Great", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Great;
-        if (GUILayout.Button("Good", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Good;
-        if (GUILayout.Button("Random", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Random;
-        if (GUILayout.Button("RandomAllPerfect", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomAllPerfect;
-        if (GUILayout.Button("RandomFullComboPlus", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomFullComboPlus;
-        if (GUILayout.Button("RandomFullCombo", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomFullCombo;
-        if (GUILayout.Button("None", MainGUI.Style.Button)) AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.None;
+
+        // 键盘控制部分AutoPlay模式，全大P/全小P没有意义所以不考虑增加
+        if (GUILayout.Button("Great", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.O))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Great;
+
+        if (GUILayout.Button("Good", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.P))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Good;
+
+        if (GUILayout.Button("Random", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.K))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.Random;
+
+        if (GUILayout.Button("RandomAllPerfect", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.G))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomAllPerfect;
+
+        if (GUILayout.Button("RandomFullComboPlus", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.H))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomFullComboPlus;
+
+        if (GUILayout.Button("RandomFullCombo", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.J))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.RandomFullCombo;
+
+        if (GUILayout.Button("None", MainGUI.Style.Button) || DebugInput.GetKeyDown(KeyCode.N))
+            AutoPlay.autoPlayMode = AutoPlay.AutoPlayMode.None;
     }
 }
