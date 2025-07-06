20aaac7ab32f86fbdc6f4402a5c192702cd6320f
WYH2004 <32890512+WYH2004-MC@users.noreply.github.com>
2025-07-06 14:29:18 +0800

    refactor: 修改自动备份的日志输出格式

--- Common/AutoBackupData.cs
+++ Common/AutoBackupData.cs
@@ -15,6 +15,6 @@ public class AutoBackupData
         UserData userData = User.GetUserData(monIndex);
         if (userData.IsGuest()) return;
         string fileName = User.ExportBackupData(monIndex, sendMessage:false);
-        MelonLogger.Msg($"[AutoBackupData] P{monIndex + 1} Player {userData.Detail.UserName}({userData.Detail.UserID}) Backup Success: {fileName} ");
+        MelonLogger.Msg($"[AutoBackupData] P{monIndex + 1} ID: {userData.Detail.UserID} Data Backup Success: {fileName} ");
     }
 }
